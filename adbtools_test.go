a4019a195808a0f33eb7acea5795c786df56623d
Hugo Virgilio <huvirgilio@gmail.com>
2020-10-26 16:00:08 -0300

    Improved tests

--- adbtools_test.go
+++ adbtools_test.go
@@ -5,24 +5,52 @@ import (
 	"testing"
 )
 
+type testData struct {
+	t      *testing.T
+	device Device
+}
+
 func TestMethods(t *testing.T) {
 	devices, err := Devices()
 	if err != nil {
 		t.Errorf("Failed to get device list: %v", err)
 		return
 	}
-	device := devices[0]
-	err = device.testScreenSize()
+	test := testData{
+		t:      t,
+		device: devices[0],
+	}
+
+	err = test.testScreenSize()
 	if err != nil {
 		t.Error(err)
 		return
 	}
+
+	err = test.testDumpPath()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
 }
 
-func (device *Device) testScreenSize() error {
-	device.ScreenSize()
-	if device.Screen.Height == 0 || device.Screen.Width == 0 {
-		return fmt.Errorf("Failed to get device screen size: %v", device.Screen)
+func (t *testData) testScreenSize() error {
+	t.t.Log("testing ScreenSize")
+	t.device.ScreenSize()
+	if t.device.Screen.Height == 0 || t.device.Screen.Width == 0 {
+		return fmt.Errorf("Failed to get device screen size: %v", t.device.Screen)
 	}
+	t.t.Log("ScreenSize test passed")
+	return nil
+}
+
+func (t *testData) testDumpPath() error {
+	t.t.Log("testing DumpPath")
+	t.device.XMLScreen(true)
+	if cleanString(t.device.Shell(fmt.Sprintf("adb shell ls %s", t.device.dumpPath))) != t.device.dumpPath {
+		return fmt.Errorf("Failed to fetch window_dump.xml")
+	}
+	t.t.Log("DumpPath test passed")
 	return nil
 }
